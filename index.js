eab54d85ef079cb68165936ce0f2f7efd729c351
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-19 02:49:21 +0300

    Реализовали вывод сокращенных названий  валют в алфавитном порядке

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const getStatistics = (content) => {
     const result = rows.slice(1, rows.length - 1);
     console.log(`Count: ${result.length}`);
 
+    // Выводит сокращенные названия валют в алфавитном порядке
+    const strs = result.map((row) => row.split(";"));
+    const letterCodes = strs.map((str) => str[3]).sort();
+    const filters = letterCodes.reduce((acc, letterCode) => {
+        if(!acc.includes(letterCode)) {
+        acc.push(letterCode);
+        };
+        return acc;
+    }, []);
+    const string = filters.join(', ');
+    console.log(`Currency codes: ${string}`);
 };
 
 export default getStatistics;
