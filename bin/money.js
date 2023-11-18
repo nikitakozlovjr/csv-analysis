1c76c67c98a16240fc5241f6ac1ba5e52864579d
nikitakozlovjr <nikita3726898@gmail.com>
2023-11-19 01:55:58 +0300

    Написали парсинг данных из переданного файла

--- bin/money.js
+++ bin/money.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log('Hello, World!');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from "node:url";
+import getStatistics from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const fileName = process.argv[2];
+
+const content = fs.readFileSync(path.join(__dirname, '..', '__fixtures__', fileName), 'utf-8');
+
+getStatistics(content);
